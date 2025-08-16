7beb454fa89a3ea8faeabd680bd43df36f77370e
zender <ender_2997587939@outlook.com>
2025-08-16 11:13:41 +0800

    ZSystem 事件脚本系统 0.1

---
+++ CSTI-ZLib/LuaLIbs/ZGameSystem.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using CSTI_LuaActionSupport.LuaCodeHelper;
+using CSTI_ZLib.Patcher;
+using CSTI_ZLib.Utils;
+
+namespace CSTI_ZLib.LuaLIbs
+{
+    [LuaLib]
+    public static class ZGameSystem
+    {
+        #region Init
+
+        internal static void LuaLibInit()
+        {
+            GamePatcher.OnPassTp += GamePatcherOnOnPassTp;
+        }
+
+        #endregion
+
+        #region Callbacks
+
+        private static void GamePatcherOnOnPassTp()
+        {
+            foreach (var card in GameManager.Instance.AllCards)
+            {
+                var uniqueID = card?.CardModel?.UniqueID;
+                if (uniqueID != null && TpSystems.TryGetValue(uniqueID, out var tpSystem))
+                {
+                    tpSystem(new CardAccessBridge(card));
+                }
+            }
+        }
+
+        #endregion
+
+        public static Dictionary<string, Action<CardAccessBridge>> TpSystems = new();
+    }
+}
---
+++ CSTI-ZLib/Patcher/GamePatcher.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using ChatTreeLoader.Util;
+using CSTI_LuaActionSupport.Helper;
+using HarmonyLib;
+
+namespace CSTI_ZLib.Patcher
+{
+    [HarmonyPatch]
+    public static class GamePatcher
+    {
+        public static event Action? OnPassTp;
+        public static List<CoroutineController> OnPassTpEnumerators = new();
+
+        [HarmonyPatch(typeof(GameManager), nameof(GameManager.ProgressCurrentResearch)), HarmonyPostfix]
+        public static void OnPassTpPatch(ref IEnumerator __result)
+        {
+            __result = __result.OnEnd(OnEnd());
+            return;
+
+            IEnumerator OnEnd()
+            {
+                OnPassTpEnumerators.Clear();
+                OnPassTp?.Invoke();
+                var coroutineQueue = GameManager.Instance.ProcessCache();
+                while (coroutineQueue.Dequeue() is { } coroutine)
+                {
+                    while (coroutine.state == CoroutineState.Running)
+                    {
+                        yield return null;
+                    }
+                }
+
+                foreach (var coroutine in OnPassTpEnumerators)
+                {
+                    while (coroutine.state == CoroutineState.Running)
+                    {
+                        yield return null;
+                    }
+                }
+            }
+        }
+    }
+}
